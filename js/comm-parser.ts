62c0555d47ad8112d0fae03434e1e854484e428d
Antonio Kam <19kamh3@gmail.com>
2019-12-21 15:52:57 +0800

    Create comm-parser.ts

---
+++ js/comm-parser.ts
@@ -0,0 +1,18 @@
+// const alg_reverse = (input_alg:string):string => {
+//     let alg = input_alg.split("")
+//     let alg_invert = Array(alg.map(x => invert(x)))
+//     return ""
+// }
+
+// const invert = (character:string):string => {
+//     if(character == "'") {
+//         return ""
+//     } else if(character == "2") {
+//         return "2'"
+//     } else if(character == "2'") {
+//         return "2"
+//     }  else {
+//         return `${character}'`
+//     }
+// }
+
